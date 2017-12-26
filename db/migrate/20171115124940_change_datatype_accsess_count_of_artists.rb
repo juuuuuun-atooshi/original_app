4a0f8ef805fc9326e2a3082c098180d52159a728
伊藤淳 <itojun@itojunMacBook-Pro.local>
2017-12-26 20:56:26 +0900

    20171226_5 commit

--- db/migrate/20171115124940_change_datatype_accsess_count_of_artists.rb
+++ db/migrate/20171115124940_change_datatype_accsess_count_of_artists.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeAccsessCountOfArtists < ActiveRecord::Migration
   def change
-    change_column :artists, :accsess_count, :integer
+    change_column :artists, :accsess_count, 'integer USING CAST(accsess_count AS integer)'
   end
 end
